3e654d271230f6072bd9ef893247b0bec9c69fb3
Gabe Ortiz <gabe@signalnine.net>
2015-08-13 15:37:11 -0700

    made some of the regex less dumb, also now alana hates sushi

--- limbo/plugins/alana.py
+++ limbo/plugins/alana.py
@@ -9,16 +9,22 @@ def downvote(username):
     response = username + "--"
     return response
 
+def sushi():
+    response = "I hate sushi."
+    return response
+
 def on_message(msg, server):
     user = server.slack.server.users.find(msg["user"])
     username = user.name
     text = msg.get("text", "")
-    match = re.findall(r"(hi|hello|Hi)( .*)?", text)
+    match = re.findall(r"^(hi|hello|Hi)\b", text)
     if match:
        return hi(username)
     match = re.findall(r"(Terry Gross|terry gross)( .*)?", text)
     if match:
        return downvote(username)
-
+    match = re.findall(r"(SUSHI|sushi)(.*)?", text)
+    if match:
+       return sushi()
 
     return
