3de5e741fcd77ad220b787f933e226ff0a9bbd2b
Gabe Ortiz <gabe@signalnine.net>
2015-08-14 15:54:01 -0700

    regex tweaks, will wasn't even born yet

--- limbo/plugins/alana.py
+++ limbo/plugins/alana.py
@@ -29,6 +29,9 @@ def random_vote(username):
     vote = ['++', "--", "++", "++"]
     return username + random.choice(vote)
 
+def will():
+    return "Will wasn't even born yet."
+
 def on_message(msg, server):
     user = server.slack.server.users.find(msg["user"])
     username = user.name
@@ -42,10 +45,13 @@ def on_message(msg, server):
     match = re.findall(r"(sushi|sashimi|tuna|maguro)(.*)?", text)
     if match:
        return sushi()
+    match = re.findall(r"\b(80s|90s|back when|long time|ago|the days|19\d{2}|20\d{2}\b)", text)
+    if match:
+       return will()
     match = re.findall(r"\b(activate|engage|dothething|initiate)(.*)?", text)
     if match:
        return beep()
-    match = re.findall(r"\b(apple|watermelon|peach|pear|banana|strawberries|cucumber|berries|leftover|pie|pizza|carrot|bean|corn|pea|nectarine|grape|donut|trash|recycle|recycling|chip|cake|cookie)e*s*\b", text)
+    match = re.findall(r"\b(apple|watermelon|peach|pear|banana|strawberr(i|y)|cucumber|berr(i|y)|leftover|pie|pizza|carrot|bean|corn|pea|nectarine|grape|donut|trash|recycle|recycling|chip|cake|cookie)e*s*\b", text)
     if match:
        return compost()
     if (random.random() < 0.05):
