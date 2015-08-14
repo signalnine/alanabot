1bf0003dad3e0e33766879f97afef013cf9503f3
Gabe Ortiz <gabe@signalnine.net>
2015-08-14 11:06:29 -0700

    tweak frequency of replies, adjust voting balance, some regex tweaks

--- limbo/plugins/alana.py
+++ limbo/plugins/alana.py
@@ -22,11 +22,11 @@ def beep():
     return response
 
 def random_response():
-    responses = ['ssanborn--', 'YAY', 'omg', ':heart:', ':smile:', 'gnats!', ':woop:', ':toot:']
+    responses = ['ssanborn--', 'YAY', 'omg', ':heart:', ':smile:', 'gnats!', ':woop:', ':toot:', ':shoe:']
     return random.choice(responses)
 
 def random_vote(username):
-    vote = ['--', '++', "++"]
+    vote = ['++', "--", "++", "++"]
     return username + random.choice(vote)
 
 def on_message(msg, server):
@@ -39,16 +39,16 @@ def on_message(msg, server):
     match = re.findall(r"(terry gross)( .*)?", text)
     if match:
        return downvote(username)
-    match = re.findall(r"(sushi)(.*)?", text)
+    match = re.findall(r"(sushi|sashimi|tuna|maguro)(.*)?", text)
     if match:
        return sushi()
     match = re.findall(r"\b(activate|engage|dothething|initiate)(.*)?", text)
     if match:
        return beep()
-    match = re.findall(r"\b(apples|watermelon|pears|bananas|strawberries|cucumbers|berries|leftovers|pie|pizza|carrots|beans|corn|peas|nectarines|grapes|donuts|trash|recycle|recycling|chips|cake|cookies)\b", text)
+    match = re.findall(r"\b(apple|watermelon|peach|pear|banana|strawberries|cucumber|berries|leftover|pie|pizza|carrot|bean|corn|pea|nectarine|grape|donut|trash|recycle|recycling|chip|cake|cookie)e*s*\b", text)
     if match:
        return compost()
-    if (random.random() < 0.01):
+    if (random.random() < 0.05):
        return random_response()
     if (random.random() < 0.01):
        return random_vote(username)
